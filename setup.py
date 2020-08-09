28590faac3f39437e67c3ece22ab4bc95d031398
fwrage <fwrage@nb-224422.fritz.box>
2020-08-09 19:12:38 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ipleak',
-      version='0.1',
+      version='0.1.1',
       description='Python Tool to check your VPN.',
       long_description=long_description,
       author='profileid',
