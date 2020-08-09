4afdb74ade80affda8249775ca4760d5825745ad
fwrage <fwrage@nb-224422.fritz.box>
2020-08-09 19:12:19 +0200

    no pypandoc anymore

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
-import pypandoc
 
-with open("./requirements.txt") as f:
-    required = f.read().splitlines()
-
-long_description = pypandoc.convert("./README.md", "rst")
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='ipleak',
       version='0.1',
