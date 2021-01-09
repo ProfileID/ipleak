3650ca723d7823dcd2bef515297b729dca9eec42
profileid <40308458+ProfileID@users.noreply.github.com>
2021-01-09 18:27:01 +0100

    testing if data is not {} not needed anymore

--- ipleak/app.py
+++ ipleak/app.py
@@ -14,33 +14,30 @@ from ipleak.functions.ipleak import ipleak_string
 def ipv4():
     with console.status('[bold green]Getting IPv4...') as status:
         data = get_url(settings.url_ipv4)
-        if data is not {}:
-            try:
-                console.print(ipleak_string("IPv4", data))
-            except NoIPFound as e:
-                console.print(f'IPv4: not available.', style='bold red')
+        try:
+            console.print(ipleak_string("IPv4", data))
+        except NoIPFound as e:
+            console.print(f'IPv4: not available.', style='bold red')
 
 
 @app.command()
 def ipv6():
     with console.status('[bold green]Getting IPv6...') as status:
         data = get_url(settings.url_ipv6)
-        if data is not {}:
-            try:
-                console.print(ipleak_string("IPv6", data))
-            except NoIPFound as e:
-                console.print(f'IPv6: not available.', style='bold red')
+        try:
+            console.print(ipleak_string("IPv6", data))
+        except NoIPFound as e:
+            console.print(f'IPv6: not available.', style='bold red')
 
 
 @app.command()
 def dns():
     with console.status('[bold green]Getting DNS...') as status:
         data = get_url(settings.url_dns)
-        if data is not {}:
-            try:
-                console.print(ipleak_string("DNS", data))
-            except NoIPFound as e:
-                console.print(f'DNS: not available.', style='bold red')
+        try:
+            console.print(ipleak_string("DNS", data))
+        except NoIPFound as e:
+            console.print(f'DNS: not available.', style='bold red')
 
 
 @app.command()
