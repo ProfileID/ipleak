f7912ea305372f86971ded728ab3fb2266595ec7
profileid <40308458+ProfileID@users.noreply.github.com>
2021-01-09 18:27:13 +0100

    v1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ipleak',
-      version='1.0.0',
+      version='1.0.1',
       description='Python Tool to check your VPN.',
       long_description=long_description,
       long_description_content_type='text/markdown',
